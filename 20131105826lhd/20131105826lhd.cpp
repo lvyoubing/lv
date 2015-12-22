929ad022c8ea18b00114e86f88a1d3eca16ca19e
lvyoubing <244252204@qq.com>
2015-12-22 22:40:56 +0800

    4.在图像上画矩形，并在文档上显示矩形坐标

--- 20131105826lhd/20131105826lhd.cpp
+++ 20131105826lhd/20131105826lhd.cpp
@@ -146,7 +146,7 @@ int main(int argc, char** argv)
 
 //3.利用OpenCV给图像添加标注
 
-#include "stdafx.h"
+/*#include "stdafx.h"
 #include "cv.h"
 #include "highgui.h"
 #include <iostream>
@@ -300,4 +300,85 @@ static void onMouse( int event, int x, int y, int, void* )
  }
  cvShowImage("吕欢达",img1);
  return;
-}
+}*/
+
+
+
+//4.利用OpenCV在图像上画矩形，并在文档上显示矩形坐标
+
+#include <opencv2/opencv.hpp>
+#include <iostream> 
+#include <cmath>
+#include <cstring> 
+using namespace std; 
+void my_mouse_callback(int event,int x,int y,int flags,void* param);//回调函数CvMouseCallback,当回调函数被调用时，opencv会传入合适的值
+//event为鼠标事件类型,x,y表示事件发生时鼠标位置的x,y坐标值。 
+//flags的值表示了别的键是否被触发。 Param可以以任何结构方式传递的参数信息。 
+CvRect box; 
+bool drawing_box=false; 
+void draw_box(IplImage* img,CvRect rect) 
+{ 
+	cvRectangle(img,cvPoint(box.x,box.y),cvPoint(box.x+box.width,box.y+box.height),cvScalar(0x00,0x00,0xff)); 
+} 
+CvFont font;
+int main()
+{  
+
+	cvInitFont(&font,CV_FONT_HERSHEY_SIMPLEX,0.4,0.4,0,1,8); //字体函数，第一个0.4是高，第二个0.4是宽。0是Shear=0，字体不倾斜
+	box=cvRect(-1,-1,0,0); 
+	IplImage* image=cvLoadImage("3.jpg"); 
+	cout<<"**"<<image->width<<" "<<image->height<<"**"<<endl;  //cvZero(image); 
+	IplImage* temp=cvCloneImage(image); 
+	cvNamedWindow("Box Example"); //产生事件的窗口
+	cvSetMouseCallback("Box Example",my_mouse_callback,(void*)image); //my_mouse_callback是回调函数
+	while(1) 
+	{   
+		cvCopy(image,temp); 
+        if(drawing_box) draw_box(temp,box);  
+		cvShowImage("Box Example",temp);   
+		if(cvWaitKey(30)==27) break; 
+    }   
+    cvSaveImage("temp.jpg",temp);  
+	cvReleaseImage(&image);  
+	cvReleaseImage(&temp); 
+    cvDestroyWindow("Box Example");  
+	return 0; 
+ } 
+void my_mouse_callback(int event,int x,int y,int flags,void* param)
+{  
+	IplImage* image=(IplImage*)param;
+	switch(event)  
+	{ 
+    case CV_EVENT_MOUSEMOVE:  
+		{   
+			if(drawing_box)   
+			{    
+				box.width=x-box.x;  
+				box.height=y-box.y; 
+            } 
+        }  
+		break; 
+	case CV_EVENT_LBUTTONDOWN: 
+        { 
+             drawing_box=true;    
+		     box=cvRect(x,y,0,0); 
+             cout<<box.x<<" "<<box.y<<endl; 
+        } 
+        break;  
+   case CV_EVENT_LBUTTONUP: 
+       {   
+		   drawing_box=false;  
+		   draw_box(image,box); 
+           cout<<box.x<<" "<<box.y<<"&&"<<box.width<<" "<<box.height<<endl;   
+		   char s1[32];  
+		   char s2[32];     
+           sprintf(s1,"(%d,%d)",box.x,box.y);    
+		   sprintf(s2,"%d",box.y); 
+           cvPutText(image,s1,cvPoint(box.x,box.y),&font,CV_RGB(0,0,255));  
+		   //文字函数，可以在图像上输出一些文本。s1所指向的文本将打印到图像上。
+		   //若将点的坐标打印到图像上，就需要把点的坐标变成字符串，要用到sprintf函数。 
+		   //box.x,box.y是矩形的左下角的x,y轴坐标
+       }   
+	   break; 
+ }
+} 
